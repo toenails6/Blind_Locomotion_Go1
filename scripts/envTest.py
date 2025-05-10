4c0bf97f7c0e2433552deb168bc07b9714c2bcb4
toenails6 <rendayang2002@163.com>
2025-05-10 00:23:25 -0400

    Environment test script updates.

--- scripts/envTest.py
+++ scripts/envTest.py
@@ -1,16 +1,12 @@
-# Import blind locomotion environment. 
-import os
-current_working_directory = os.getcwd()
-print(f"Current working directory: {current_working_directory}")
-from source.Blind_Locomotion_Go1.Blind_Locomotion_Go1.tasks.manager_based.blind_locomotion_go1.blind_locomotion_go1_env_cfg import UnitreeGo1_BlindLocomotionEnvCfg
-
-# Isaaclab pseudo command line imports. 
+# Pseudo command line. 
 import argparse
 
+# Import isaac lab and sim app launcher. 
 from isaaclab.app import AppLauncher
 
 # add argparse arguments
 parser = argparse.ArgumentParser(description="Quadruped blind locomotion environment. ")
+parser.add_argument("--task", type=str, default="Isaac-Velocity-Rough-Unitree-Go1-v0", help="Name of the task/environment.")
 parser.add_argument("--num_envs", type=int, default=1, help="Number of environments to spawn.")
 
 # append AppLauncher cli args
@@ -23,15 +19,24 @@ app_launcher = AppLauncher(args_cli)
 simulation_app = app_launcher.app
 
 # Library imports. 
-from isaaclab.envs import ManagerBasedRLEnv
+from isaaclab_tasks.utils import parse_env_cfg
+import gymnasium as gym
 import torch
 
 
 def main(): 
-    print("Testing blind locomotion environment for Unitree Go1. ")
-    envCfg = UnitreeGo1_BlindLocomotionEnvCfg()
-    envCfg.scene.num_envs = args_cli.num_envs
-    env = ManagerBasedRLEnv(cfg=envCfg)
+    print("Preview blind locomotion environment for Unitree Go1. ")
+    # Create the environment configuration. 
+    envCfg = parse_env_cfg(
+        args_cli.task, 
+        num_envs=args_cli.num_envs
+    )
+
+    # Create the environment. 
+    env = gym.make(args_cli.task, cfg=envCfg)
+
+    # Reset the environment
+    env.reset()
 
     # Environment and robot preview. 
     count = 0
@@ -45,10 +50,10 @@ def main():
                 print("[INFO]: Resetting environment...")
 
             # Sample random actions. 
-            joint_vel = torch.randn_like(env.action_manager.action)
+            actions = torch.randn_like(env.action_manager.action)
 
             # Step the environment. 
-            obs, rew, terminated, truncated, info = env.step(joint_vel)
+            obs, rew, terminated, truncated, info = env.step(actions)
 
             # Legacy cartpole print views. 
             # # Print current orientation of pole
@@ -57,10 +62,10 @@ def main():
             # Update counter. 
             count += 1
 
-    # Close the environment. 
+    # Close the environment and simulation app. 
     env.close()
+    simulation_app.close()
 
 
 if __name__ == "__main__":
     main()
-    simulation_app.close()
